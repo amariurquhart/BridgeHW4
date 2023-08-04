0923a962ddc5381e16254939921e150a8dd7d9a4
Amari <amari.urquhart@gmail.com>
2023-08-04 04:19:16 -0400

    touch up

--- au2261_hw4_q7.cpp
+++ au2261_hw4_q7.cpp
@@ -11,7 +11,7 @@ main() {
     for (ny = 1; ny <= userEntry; ny++) {
         for (nx = 1; nx <= userEntry; nx++) {
             temp2 = (nx * ny);
-            cout << temp2 << "    ";
+            cout << temp2 << "\t";
             }
         cout << endl;
         }
